621067da2db58dd7a2b19e0bb005876591664aa5
Excel <000.excel@gmail.com>
2020-06-01 20:09:21 +0530

    Add: Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Shop'),
+        ),
+        body: Center(
+          child: Text('My Shop'),
+        ),
+      ),
+    );
+  }
+}
