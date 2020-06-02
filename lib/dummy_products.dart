1927a25373de55866e6f8c3294c4936d0220de81
Excel <000.excel@gmail.com>
2020-06-02 09:00:30 +0530

    TestAdd: Dummy Data Added

---
+++ lib/dummy_products.dart
@@ -0,0 +1,20 @@
+import './models/product.dart';
+
+final List<Product> dummyProducts = [
+  Product(
+    id: 'p1',
+    title: 'Red Shirt',
+    description: 'A red shirt - it is pretty red!',
+    price: 29.99,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+  ),
+  Product(
+    id: 'p2',
+    title: 'Trousers',
+    description: 'A nice pair of trousers.',
+    price: 59.99,
+    imageUrl:
+        'https://image.shutterstock.com/image-photo/girls-beautiful-yellow-skinny-trousers-600w-712737529.jpg',
+  ),
+];
