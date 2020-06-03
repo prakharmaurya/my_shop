a2a963afc81bce5bde396b7cba33b7cac4db7ac5
Excel <000.excel@gmail.com>
2020-06-03 20:53:58 +0530

    Add: Form image preview and enter user field added

--- lib/screens/edit_product_screen/edit_product_screen.dart
+++ lib/screens/edit_product_screen/edit_product_screen.dart
@@ -7,6 +7,33 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFoucsNode = FocusNode();
+  final _descFoucsNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFoucsNode = FocusNode();
+
+  @override
+  void initState() {
+    _imageUrlFoucsNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _imageUrlFoucsNode.removeListener(_updateImageUrl);
+    _priceFoucsNode.dispose();
+    _descFoucsNode.dispose();
+    _imageUrlFoucsNode.dispose();
+
+    super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFoucsNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +50,65 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   labelText: 'Title',
                 ),
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFoucsNode);
+                },
+              ),
+              TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Price',
+                  ),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  focusNode: _priceFoucsNode,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_descFoucsNode);
+                  }),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                ),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descFoucsNode,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Enter image URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _imageUrlController.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Image URL',
+                      ),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFoucsNode,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
