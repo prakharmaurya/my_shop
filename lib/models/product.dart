dadb5cf0d2b0f5c5ef3f2f471099310e5681e4a6
Excel <000.excel@gmail.com>
2020-06-04 14:42:56 +0530

    Add: Favourite synced to firebase

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,23 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = 'https://shop-app-f3190.firebaseio.com/products/$id.json';
+    try {
+      final res = await http.patch(
+        url,
+        body: json.encode({'isFavorite': isFavorite}),
+      );
+      if (res.statusCode >= 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+      }
+    } catch (err) {
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
   }
 }
