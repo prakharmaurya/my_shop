c21572308bee82d23de657f95a92fb3c107c6ea1
Excel <000.excel@gmail.com>
2020-06-04 15:32:59 +0530

    Add: Order fecth fn added

--- lib/screens/orders_screen/oreders_screen.dart
+++ lib/screens/orders_screen/oreders_screen.dart
@@ -5,8 +5,30 @@ import '../../providers/orders_provider.dart';
 import './widgets/order_item_view.dart';
 import '../../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  bool _isLoading = false;
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<OrdersProvider>(context, listen: false)
+          .fetchAndSetOrders();
+
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +39,14 @@ class OrdersScreen extends StatelessWidget {
         title: Text('Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return OrderItemView(orderData.orders[index]);
-        },
-        itemCount: orderData.orders.length,
-      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return OrderItemView(orderData.orders[index]);
+              },
+              itemCount: orderData.orders.length,
+            ),
     );
   }
 }
