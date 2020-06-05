dff06dde43d43e1b98def28f84b79af62fcb5d04
Excel <000.excel@gmail.com>
2020-06-05 19:43:29 +0530

    Add : Animated height via AnimatedBuilder

--- lib/screens/auth_screen/auth_screen.dart
+++ lib/screens/auth_screen/auth_screen.dart
@@ -121,7 +121,6 @@ class _AuthCardState extends State<AuthCard>
         .animate(
       CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
     );
-    _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -208,12 +207,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+            // height: _authMode == AuthMode.Signup ? 320 : 260,
+            height: _heightAnimation.value.height,
+            constraints:
+                BoxConstraints(minHeight: _heightAnimation.value.height),
+            width: deviceSize.width * 0.75,
+            padding: EdgeInsets.all(16.0),
+            child: ch),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
